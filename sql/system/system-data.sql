b1f49501635c5dceeed13cdc5b42e41b4452a19a
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2023-03-31 23:12:01 +0000

    Updated system query to include ip address from mmb_config col

--- sql/system/system-data.sql
+++ sql/system/system-data.sql
@@ -1,1 +1,9 @@
-SELECT id, hhm_config, hhm_file_config from systems WHERE hhm_config->>'data_acqu' = 'mmb';
+SELECT
+    id,
+    hhm_config,
+    hhm_file_config,
+    mmb_config -> 'ssh' -> 'ip_address' AS ip_address
+from
+    systems
+WHERE
+    hhm_config ->> 'data_acqu' = 'mmb';
