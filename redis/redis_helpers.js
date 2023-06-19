e8ea0f80f4275a376db41f0c2def2e9bea448cd9
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2023-06-19 14:23:48 +0000

    Refactor to accept dir name

--- redis/redis_helpers.js
+++ redis/redis_helpers.js
@@ -2,24 +2,14 @@ const { log } = require("../logger");
 const initRedis = require("./redis_instance");
 const fs = require("node:fs").promises;
 
-async function update_last_dir_date(sme, path) {
+async function update_last_dir_date(sme, directory) {
   const redisClient = await initRedis(
     process.env.REDIS_PORT,
     process.env.REDIS_IP
   );
   try {
-    const files_in_dir = await fs.readdir(path);
-    console.log("\nfiles_in_dir");
-    console.log(files_in_dir);
-
-    const last_dir_date = files_in_dir[files_in_dir.length - 1].split("_");
-
-    console.log(last_dir_date);
-
-    await redisClient.quit();
-    return;
-    const setKey = `${sme}.phil_cv_date`;
-    const setValue = date;
+    const setKey = `${sme}.last_phil_cv_dir`;
+    const setValue = directory;
     await redisClient.set(setKey, setValue);
     await redisClient.quit();
   } catch (error) {
