967dca6169c89ec442bd448a58120ae1824dc46e
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2023-07-19 15:26:10 +0000

    Update Phil CV script to pull Logger.mdb. Was pulling Output.mdb

--- read/sh/Philips/phil_ct_data_grab_1.sh
+++ read/sh/Philips/phil_ct_data_grab_1.sh
@@ -1,20 +1,20 @@
 # set -ue
 [ ! -d "$4" ] && mkdir $4
 
-lftp -c "set net:timeout 20; set ftp:ssl-allow off; set net:reconnect-interval-base 5; set net:max-retries 2; set sftp:connect-program 'ssh -oKexAlgorithms=diffie-hellman-group14-sha1'; open sftp://$2:$3@$1; cd /cygdrive/d/Data_Logger/; mget -e Output.mdb -O $4; exit"
+lftp -c "set net:timeout 20; set ftp:ssl-allow off; set net:reconnect-interval-base 5; set net:max-retries 2; set sftp:connect-program 'ssh -oKexAlgorithms=diffie-hellman-group14-sha1'; open sftp://$2:$3@$1; cd /cygdrive/d/Data_Logger/; mget -e Logger.mdb -O $4; exit"
 if [ $? -ne 0 ]; then
     echo "Connection timed out" >&2
     exit
 fi
 
-chmod +0644 $4/Output.mdb
+chmod +0644 $4/Logger.mdb
 # { BEGIN CONVERSION
 now_ISO8601=$(date -u +"%Y-%m-%dT%H:%M:00Z")
 
-test "$(ls -l $4/Output.mdb | awk '{print $5}')" -gt "0" && chmod +0644 $4/Output.mdb
+test "$(ls -l $4/Logger.mdb | awk '{print $5}')" -gt "0" && chmod +0644 $4/Logger.mdb
 
-EALINFO_TABLEVAR=$(mdb-tables $4/Output.mdb | sed 's/ /\n/g' | sed -n 1p)
-EVENTS_TABLEVAR=$(mdb-tables $4/Output.mdb | sed 's/ /\n/g' | sed -n 2p)
+EALINFO_TABLEVAR=$(mdb-tables $4/Logger.mdb | sed 's/ /\n/g' | sed -n 1p)
+EVENTS_TABLEVAR=$(mdb-tables $4/Logger.mdb | sed 's/ /\n/g' | sed -n 2p)
 # { ACCOUNT FOR EXISTING LOGS
 touch $4/Events.output
 test "$(ls -l $4/Events.output | awk '{print $5}')" -gt "0" && echo -n "" >$4/Events.output
@@ -30,13 +30,13 @@ echo "Type,Level,Module,TStampNum,DataTime,Msg,EAL,BLOB,EventTime" >>$4/Events.o
 # SORT REORDERED COLUMNS
 # REMOVE ALL NON-COMPLIANT ROWS
 # OUTPUT FILE
-mdb-export $4/Output.mdb $EVENTS_TABLEVAR | tr -cd [:alnum:]'\n''\ ''"'',''['']'':''\-''_''.''/''=' | awk -v FS="," '{print $9, $1, $2, $3, $4, $5, $6, $7, $8}' | sort | sed -n -e "/^\"[0-9][0-9]\/.*/p" >>$4/Events.output
+mdb-export $4/Logger.mdb $EVENTS_TABLEVAR | tr -cd [:alnum:]'\n''\ ''"'',''['']'':''\-''_''.''/''=' | awk -v FS="," '{print $9, $1, $2, $3, $4, $5, $6, $7, $8}' | sort | sed -n -e "/^\"[0-9][0-9]\/.*/p" >>$4/Events.output
 #echo "[/reading] : $EVENTS_TABLEVAR" >> /opt/files/$PATH/Events.output
 #echo "[END CAPTURE BLOCK : ${now_ISO8601}]" >> /opt/files/$PATH/Events.output
 #echo "[START CAPTURE BLOCK : ${now_ISO8601}]" >> /opt/files/$PATH/EALInfo.output
 #echo "[reading] : $EALINFO_TABLEVAR" >> /opt/files/$PATH/EALInfo.output
 # MAY HAVE TO FUDGE HEADERS IN BELOW THIS COMMENTED LINE
 echo "DTime,Controller,DataType,LogNumber,TmStamp,ERR_TYPE,ErrNum,vxwErrNo,File,Line,Param1,Param2,Info,EalTime" >>$4/EALInfo.output
-mdb-export $4/Output.mdb $EALINFO_TABLEVAR | tail -n +2 | sort | awk 'NF > 2' >>$4/EALInfo.output
+mdb-export $4/Logger.mdb $EALINFO_TABLEVAR | tail -n +2 | sort | awk 'NF > 2' >>$4/EALInfo.output
 
 exit
