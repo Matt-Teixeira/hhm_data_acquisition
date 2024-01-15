edf901685241ec2739ba32c8ec96d716e5db8498
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2024-01-15 14:57:00 +0000

    Set xfer clobber to true

--- read/sh/Philips/philips_ct_data_grab_ftp.sh
+++ read/sh/Philips/philips_ct_data_grab_ftp.sh
@@ -1,7 +1,7 @@
 # set -ue
 [ ! -d "$4" ] && mkdir $4
 
-lftp -c "set net:timeout 5; set ftp:ssl-allow off; set net:reconnect-interval-base 5; set net:max-retries 2; open ftp://$2:$3@$1; 
+lftp -c "set net:timeout 5; set ftp:ssl-allow off; set net:reconnect-interval-base 5; set net:max-retries 2; set xfer:clobber true; open ftp://$2:$3@$1; 
 cd /D:/Data_Logger/; 
 mget Logger.mdb -O $4; 
 exit"
